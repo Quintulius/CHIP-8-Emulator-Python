c3f263ada78e760ac79241b080e54864b852a2f6
Quintulius <quentin.oliveau@gmail.com>
2016-05-29 14:18:16 +0200

    Update screen.py

--- screen.py
+++ screen.py
@@ -2,31 +2,23 @@ import numpy as np
 from pygame.surfarray import make_surface
 
 
-class Pixel:
-    def __init__(self, _pos, _color):
-        self.color = _color
-        self.pos = _pos
-
-
 class Screen:
     def __init__(self, _w, _h, _scale):
         self.height = _h
         self.width = _w
         self.scale = _scale
-        self.white_pixel = make_surface(np.ones((self.scale, self.scale), dtype=int)*255)
-        self.black_pixel = make_surface(np.zeros((self.scale, self.scale), dtype=int))
-        self.surface_pixels = {0: self.black_pixel, 1: self.white_pixel}
-        self.pixels_matrix = []
-        self.to_redraw = []
+        white_pixel = make_surface(np.ones((self.scale, self.scale), dtype=int)*255)
+        black_pixel = make_surface(np.zeros((self.scale, self.scale), dtype=int))
+        self.surface_pixels = {0: black_pixel, 1: white_pixel}
+        self.pixels_matrix = None
+        self.to_redraw = None
         self.clear()
 
     def clear(self):
-        self.pixels_matrix = [[Pixel((i*self.scale, j*self.scale), 0) for j in range(self.height)]
-                              for i in range(self.width)]
+        self.pixels_matrix = np.zeros((self.width, self.height), dtype=int)
         self.to_redraw = [(i, j) for j in range(self.height) for i in range(self.width)]
 
     def redraw(self, background):
         for pos in self.to_redraw:
-            pixel = self.pixels_matrix[pos[0]][pos[1]]
-            background.blit(self.surface_pixels[pixel.color], pixel.pos)
+            background.blit(self.surface_pixels[self.pixels_matrix[pos]], (pos[0]*self.scale, pos[1]*self.scale))
         self.to_redraw = []
